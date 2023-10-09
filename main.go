59a5b1cda0f360920d85714d39041e024909a211
瓦苏 <vasu.shori30@gmail.com>
2023-10-09 11:15:05 +0530

    how to add value inside struct

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ type person struct {
 
 func main() {
 	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
 	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", alex) // %+v will print out the field names as well
 }
